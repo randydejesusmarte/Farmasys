3f4a5d56d7bc0dae6990574c448d433b70263736
Randy de Jesus Marte Diaz <72465717+randydejesusmarte@users.noreply.github.com>
2021-02-24 10:35:43 -0400

    Add files via upload

--- WindowsFormsApp1/WindowsFormsApp1/login.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/login.Designer.cs
@@ -122,8 +122,8 @@
             // 
             // login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(239, 363);
             this.Controls.Add(this.linkLabel2);
@@ -134,8 +134,9 @@
             this.Controls.Add(this.txtpass);
             this.Controls.Add(this.txtname);
             this.Controls.Add(this.pictureBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
